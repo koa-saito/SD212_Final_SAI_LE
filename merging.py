708162f38a3529924d834b560e5456577dfff1f3
Dean Le <deanle.cali@gmail.com>
2026-04-16 08:15:09 -0400

    modified merging

--- merging.py
+++ merging.py
@@ -3,9 +3,12 @@ import pandas as pd
 lobby = pd.read_csv("data/raw/LobbyAmounts.csv")
 awards = pd.read_csv("data/raw/Transactions.csv")
 
-print(awards[['federal_action_obligation']])
-
 merged = pd.merge(lobby,awards.rename(columns={"action_date_fiscal_year":"Year"}),on="Year", how="outer")
 merged = merged[['Year','Lobby Amount','federal_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award']]
-merged = merged.iloc[merged['Lobby Amount'].isna==False]
+merged = merged.loc[merged['Lobby Amount']>0]
+merged = merged.loc[merged['federal_action_obligation']>0]
+merged = merged.loc[merged['total_obligated_amount']>0]
+merged = merged.loc[merged['total_outlayed_amount_for_overall_award']>0]
+
+
 print(merged)
