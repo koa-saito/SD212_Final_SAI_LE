01e7453556ee5872e5e4fe6d5260eda21cae8135
Dean Le <deanle.cali@gmail.com>
2026-04-16 08:04:04 -0400

    trying to add new file

--- merging.py
+++ merging.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-lobby = pd.read_csv("LobbyingMon.csv")
-awards = pd.read_csv("Transactions")
+lobby = pd.read_csv("data/raw/LobbyAmounts.csv")
+awards = pd.read_csv("data/raw/Transactions.csv")
+
+print(awards[['federal_action_obligation']])
 
 merged = pd.merge(lobby,awards.rename(columns={"action_date_fiscal_year":"Year"}),on="Year", how="outer")
-merged = merged[['feder_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award',]]
+merged = merged[['Year','Lobby Amount','federal_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award']]
+merged = merged.iloc[merged['Lobby Amount'].isna==False]
+print(merged)
