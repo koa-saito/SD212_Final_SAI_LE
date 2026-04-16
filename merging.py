b96a0cf8ab7dbfb6934773475ebc1812335a73f7
Dean Le <deanle.cali@gmail.com>
2026-04-16 08:29:09 -0400

    modifying merging file fs

--- merging.py
+++ merging.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 lobby = pd.read_csv("data/raw/LobbyAmounts.csv")
 awards = pd.read_csv("data/raw/Transactions.csv")
+print(awards['cfda_title'])
 
 merged = pd.merge(lobby,awards.rename(columns={"action_date_fiscal_year":"Year"}),on="Year", how="outer")
-merged = merged[['Year','Lobby Amount','federal_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award']]
+merged = merged[['Year','Lobby Amount','federal_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award','cfda_title']]
 merged = merged.loc[merged['Lobby Amount']>0]
 merged = merged.loc[merged['federal_action_obligation']>0]
 merged = merged.loc[merged['total_obligated_amount']>0]
