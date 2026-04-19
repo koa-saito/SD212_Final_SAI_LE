83fb77660c5e7b17405625dcd9f615281acae857
Dean Le <deanle.cali@gmail.com>
2026-04-19 17:31:47 -0400

    there was an attempt

--- merge1.py
+++ merge1.py
@@ -1,9 +1,22 @@
 import pandas as pd
 
-lobbycsv = pd.read_csv('data/raw/Top Contributors, 2015-2016.csv')
+lobby1csv = pd.read_csv('data/raw/Top Contributors, 2015-2016.csv')[['Contributor','To Dems','To Repubs','Lean']]
+lobby2csv = pd.read_csv('data/raw/Top Contributors, 2019-2020.csv')[['Contributor','To Dems','To Repubs','Lean']]
+lobby3csv = pd.read_csv('data/raw/Top Contributors, 2023-2024.csv')[['Contributor','To Dems','To Repubs','Lean']]
 grantcsv = pd.read_csv('data/raw/Transactions.csv')
+maj = pd.read_csv('data/raw/HORMaj.csv')
 
-print(grantcsv['recipient_name'])
+matchedMajority = pd.merge(maj,grantcsv.rename(columns={"action_date_fiscal_year":"Year"}),on="Year", how="right")
+
+matchedMajority = matchedMajority[['Year','Political Majority','recipient_name','total_obligated_amount']]
+
+matchedMajority['loweredName'] = matchedMajority['recipient_name'].str.lower()
+lobby1csv['loweredName'] = lobby1csv['Contributor'].str.lower
+print(matchedMajority)
+
+mergedTop = pd.merge(matchedMajority,lobby1csv,on="loweredName", how="inner")
+
+print(mergedTop)
 
 
 
--- merging.py
+++ merging.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-lobby = pd.read_csv("data/raw/LobbyAmounts.csv")
+# lobby = pd.read_csv("data/raw/LobbyAmounts.csv")
 awards = pd.read_csv("data/raw/Transactions.csv")
 print(awards['cfda_title'])
 
-merged = pd.merge(lobby,awards.rename(columns={"action_date_fiscal_year":"Year"}),on="Year", how="outer")
-merged = merged[['Year','Lobby Amount','federal_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award','cfda_title']]
-merged = merged.loc[merged['Lobby Amount']>0]
-merged = merged.loc[merged['federal_action_obligation']>0]
-merged = merged.loc[merged['total_obligated_amount']>0]
-merged = merged.loc[merged['total_outlayed_amount_for_overall_award']>0]
+# merged = pd.merge(lobby,awards.rename(columns={"action_date_fiscal_year":"Year"}),on="Year", how="outer")
+# merged = merged[['Year','Lobby Amount','federal_action_obligation','total_obligated_amount','total_outlayed_amount_for_overall_award','cfda_title']]
+# merged = merged.loc[merged['Lobby Amount']>0]
+# merged = merged.loc[merged['federal_action_obligation']>0]
+# merged = merged.loc[merged['total_obligated_amount']>0]
+# merged = merged.loc[merged['total_outlayed_amount_for_overall_award']>0]
 
 
-print(merged)
+# print(merged)
